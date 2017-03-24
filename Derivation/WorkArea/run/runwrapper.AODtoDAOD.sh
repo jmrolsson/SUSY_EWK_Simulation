3dc29d2d1823a593c684afb9f386f65926729d78
Mats Joakim Robert Olsson <jolsson@lxplus115.cern.ch>
2017-03-24 22:07:05 +0100

    Added additional scripts for simulation and reco

--- Derivation/WorkArea/run/runwrapper.AODtoDAOD.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-# Customised environment
-athena.py --preloadlib=/afs/cern.ch/sw/IntelSoftware/linux/x86_64/xe2013/composer_xe_2013.3.163/compiler/lib/intel64/libintlc.so.5:/afs/cern.ch/sw/IntelSoftware/linux/x86_64/xe2013/composer_xe_2013.3.163/compiler/lib/intel64/libimf.so --drop-and-reload runargs.AODtoDAOD.py PATJobTransforms/skeleton.AODtoDAOD_tf.py
