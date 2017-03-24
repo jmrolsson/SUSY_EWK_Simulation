46dd6cb513a5eac58c75d7ce4e766a3e1c7744c9
Joakim Olsson <joakim.olsson@cern.ch>
2017-03-24 22:30:13 +0100

    Removed junk

--- AFII/runwrapper.EVNTtoDAOD.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-# Customised environment
-athena.py --preloadlib=/cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc6-gcc49-opt/20.7.5/sw/IntelSoftware/linux/x86_64/xe2013/composer_xe_2013.3.163/compiler/lib/intel64/libintlc.so.5:/cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc6-gcc49-opt/20.7.5/sw/IntelSoftware/linux/x86_64/xe2013/composer_xe_2013.3.163/compiler/lib/intel64/libimf.so --drop-and-reload runargs.EVNTtoDAOD.py PATJobTransforms/skeleton.AODtoDAOD_tf.py
